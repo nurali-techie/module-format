40ed6eb46638eaa5c39f6efd4ff3c140d292a101
nurali <nurali.virani@go-jek.com>
2020-06-19 22:12:41 +0530

    add docs

--- format.go
+++ format.go
@@ -2,6 +2,7 @@ package format
 
 import "strings"
 
+// Name returns given name after triming spaces and making it title case.
 func Name(name string) string {
 	name = strings.TrimSpace(name)
 	name = strings.Title(name)
